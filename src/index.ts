4cf75fdaa453ad66f6f85d7e8636ceff1e0d1e2f
velikzhanin-av <a.velikzhanin90@gmail.com>
2024-03-01 11:58:18 +0300

    added env port

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.port || 3000
 
 app.get('/', (req: any, res: any) => {
     res.send("Hello World")

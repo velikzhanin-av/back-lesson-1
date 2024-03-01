0d344d23309c6d8b4f6b164106da4928ef26772b
velikzhanin-av <a.velikzhanin90@gmail.com>
2024-03-01 12:25:23 +0300

    fix verCel

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 const express = require('express')
-// import {config} from "dotenv";
+import {config} from "dotenv";
 
 const app = express()
 
-// config()
-const PORT =  3000
+config()
+const PORT = process.env.port || 3000
 
-app.get('/', (req, res) => {
+app.get('/', (req: any, res: any) => {
     res.send("Hello World")
 })
 

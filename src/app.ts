adeb125b7f5d01767a1c08b11a485f8b21881f71
velikzhanin-av <a.velikzhanin90@gmail.com>
2024-03-02 11:49:38 +0300

    added test end point

--- src/app.ts
+++ src/app.ts
@@ -5,3 +5,11 @@ export const app = express()
 app.get('/', (req: any, res: any) => {
     res.send("Hello World")
 })
+
+app.use('/hometask_01/api/testing/all-data', (req: any, res: any) => {
+    res.json({"delete all": "deg"})
+})
+
+app.get('/about', (req, res) => {
+    res.send('about')
+})

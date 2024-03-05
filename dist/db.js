389d13f1ca95c4101ec01a4b371e6898966c447c
velikzhanin-av <a.velikzhanin90@gmail.com>
2024-03-05 09:51:53 +0300

    del

--- dist/db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createDB = void 0;
-const createDB = () => {
-    return {
-        videos: []
-    };
-};
-exports.createDB = createDB;

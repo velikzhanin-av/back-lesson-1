a674620c6f3da6bcafe488c06d2efa67c667ffdd
velikzhanin-av <a.velikzhanin90@gmail.com>
2024-03-05 09:37:03 +0300

    fix all routes not found

--- src/routes/videos/routes.ts
+++ src/routes/videos/routes.ts
@@ -2,16 +2,10 @@ import {SETTINGS} from "../../settings";
 import {app} from "../../app";
 import {getVideos} from "../../controllers/videos/getVideos";
 import {createVideo} from "../../controllers/videos/createVideo";
+import {deleteAll} from "../../controllers/videos/deleteAll";
 
 
-
-export const deleteAll = (req: any, res: any) => {
-    res
-        .status(204)
-        .json({result: 'All data is deleted'})
-}
-
-
+app.get('/', (req: any, res: any) => {res.send('hello world')})
 app.delete('/testing/all-data', deleteAll)
 app.get(SETTINGS.PATH.VIDEOS, getVideos)
 app.post(SETTINGS.PATH.VIDEOS, createVideo)
